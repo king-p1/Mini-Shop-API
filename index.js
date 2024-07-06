8c2093fed612a258fe978cd4ee462f405ba090f5
king-p1 <98545430+king-p1@users.noreply.github.com>
2024-07-06 19:04:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use((req, res, next) =>{
 
 const PORT = process.env.PORT
 app.get('/',  async (req, res) => {
-    res.json({msg:`Mini Shop API is Live `})
+    res.json({msg:`Mini Shop API is Live`})
 })
 
 app.use('/api/v1/products',productRoutes)
